55d485d78123f207a7b391df2f951302a29d0b6a
Hallvard Haugen <hallvakh@stud.ntnu.no>
2018-02-26 19:40:30 +0100

    Added functionality for receiving an owners work

--- contracts/WorkBase.sol
+++ contracts/WorkBase.sol
@@ -1,47 +1,43 @@
-pragma solidity ^0.4.19;
+pragma solidity ^0.4.0;
 
 
-//base contract defining basic structs and functions for works
 contract WorkBase {
-
-  //every musical work is represented by a struct with a set of variables
+//every musical work is represented by a struct with a set of variables
     struct Work {
-    //timestamp from the block when this work was registered
+
+      //timestamp from the block when this work was registered
         uint64 birthTime;
 
-    //description of work, i.e "musical composition" or "sound recording of blablabla"
+        //description of work, i.e "musical composition" or "sound recording of blablabla"
         string typeOfWork;
 
-    //the Keccak-256 hash fingerprint of the uploaded file
+        //the Keccak-256 hash fingerprint of the uploaded file
         uint fingerprint;
 
-    //list of contributors address
+        //list of contributors address
         address[] contributors;
 
-    //list of percentage of owned works
+        //list of percentage of owned works
         uint[] splits;
 
     }
 
-  //An array containing the work struct for all registered works in existence.
-  //the index of the struct in the array is the ID of the work
-    Work[] private workDB;
+    //An array containing the work struct for all registered works in existence.
+    //the index of the struct in the array is the ID of the work
+    Work[] public workDB;
 
-  //a master database of all issued copyright tokens
-  //whenever a work is created, 100 copyright tokens are created and added to this array,
-  //with their index in the array becoming the copyright token's ID.
-    uint[] private rcnDB;
+    //a master database of all issued copyright tokens
+    //whenever a work is created, 100 copyright tokens are created and added to this array,
+    //with their index in the array becoming the copyright token's ID.
+    uint[] public rcnDB;
 
-  // mapping from tokenID to the address of its owner
-    mapping(uint => address) private tokenIdToOwner;
+    // mapping from tokenID to the address of its owner
+    mapping(uint => address) public tokenIdToOwner;
 
-    function createWork (string _typeOfWork,
-        uint _fingerprint,
-        address[] _contributors,
-        uint[] _splits) private {
+    //mapping from address (owner) to an array of workIds
+    mapping(address => uint[]) public addressRelatedToWork;
 
-          //her trenger vi masse require(blablabla);
-
+    function createWork (string _typeOfWork, uint _fingerprint, address[] _contributors, uint[] _splits) public {
         Work memory _newWork = Work({
             birthTime: uint64(now),
             typeOfWork: _typeOfWork,
@@ -52,15 +48,41 @@ contract WorkBase {
 
         uint newWorkId = workDB.push(_newWork) - 1;
 
-    // require contributors.length <= 100
+        // require contributors.length <= 100
 
-        for (uint i = 0; i < _contributors.length; i++) {
-            for (uint j = 0; j < _splits[i]; j++) {
+        for (uint i= 0; i < _contributors.length; i++) {
+            addressRelatedToWork[_contributors[i]].push(newWorkId);
+            for (uint j= 0; j < _splits[i]; j++) {
                 uint newtokenId = rcnDB.push(newWorkId) - 1;
                 tokenIdToOwner[newtokenId] = _contributors[i];
             }
         }
-
     }
 
+    function getOwnersWork (address _address) public view returns(uint[], uint[]) {
+        //Create a temporary array that an address is associated with.
+        //The mapping from address to an array of workIds returns an array
+        //that we can set our temporary list equal to.
+        uint[] memory workList = addressRelatedToWork[_address];
+
+        //Create an empty array of the same size as workList that will
+        // hold the split an owner (address) has of a specific work.
+        uint[] memory amountList = new uint[](workList.length);
+
+        //For every work the address is related to we add the corresponding split amount
+        //to the amountArray.
+        for (uint i = 0; i < workList.length; i++) {
+            Work memory _work = workDB[workList[i]];
+            address[] memory _contributors = _work.contributors;
+
+            for (uint j = 0; j < _contributors.length; j++) {
+                if (_contributors[j] == _address) {
+                    //Adding the corresponding split to the same index as workList
+                    amountList[i] = _work.splits[j];
+                    break;
+                }
+            }
+        }
+        return (workList, amountList);
+    }
 }
